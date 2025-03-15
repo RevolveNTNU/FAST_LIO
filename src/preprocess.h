cb9c813428c302e94c322e1bf8293287d5a7d3a8
Andreas <andreas.rodahl@revolve.no>
2025-03-15 16:40:46 +0100

    trasnform the remaining ring fieldd

--- src/preprocess.h
+++ src/preprocess.h
@@ -107,7 +107,7 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(ouster_ros::Point,
     // use std::uint32_t to avoid conflicting with pcl::uint32_t
     (std::uint32_t, t, t)
     (std::uint16_t, reflectivity, reflectivity)
-    (std::uint8_t, ring, ring)
+    (std::uint16_t, ring, ring)
     (std::uint16_t, ambient, ambient)
     (std::uint32_t, range, range)
 )
